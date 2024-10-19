e63ecd6faf273cf28742f64d0d0de333fd86e429
Sunly <129272504+Sunly24@users.noreply.github.com>
2024-10-19 15:57:28 +0700

    W2-S2

--- W2-S2-PRACTICE/EX-2.dart
+++ W2-S2-PRACTICE/EX-2.dart
@@ -1,31 +1,78 @@
 class BankAccount {
-    // TODO
+  final int accountId;
+  final String accountOwer;
+  double _balance = 0;
+
+  BankAccount(this.accountId, this.accountOwer);
+
+  double get getBalance => _balance;
+
+  double balance() {
+    return _balance;
+  }
+
+  void withdraw(double amount) {
+    if (amount > 1) {
+      if (_balance >= amount) {
+        _balance -= amount;
+      } else {
+        throw Exception('Insufficient balance for withdraw!');
+      }
+    } else {
+      throw Exception('Withdraw amount must be start from \$1');
+    }
+  }
+
+  void credit(double amount) {
+    if (amount >= 1) {
+      _balance += amount;
+    } else {
+      throw Exception('Credit amount must be start from \$1');
+    }
+  }
 }
 
 class Bank {
-    // TODO
+  final String name;
+  final String location;
+  final List<BankAccount> _accounts = [];
+
+  Bank(this.name, this.location);
+
+  BankAccount createAccount(int accountId, String accountOwner) {
+    for (var account in _accounts) {
+      if (account.accountId == accountId) {
+        throw Exception('Account have already exists');
+      }
+    }
+
+    var newAccount = BankAccount(accountId, accountOwner);
+    _accounts.add(newAccount);
+    return newAccount;
+  }
 }
- 
+
 void main() {
+  Bank myBank = Bank('CADT Bank', 'Phnom Penh');
 
-  // Bank myBank = Bank(name: "CADT Bank");
-  // BankAccount ronanAccount = myBank.createAccount(100, 'Ronan');
+  BankAccount ronanAccount = myBank.createAccount(100, 'Ronan');
+  print(ronanAccount.balance());
 
-  // print(ronanAccount.balance); // Balance: $0
-  // ronanAccount.credit(100);
-  // print(ronanAccount.balance); // Balance: $100
-  // ronanAccount.withdraw(50);
-  // print(ronanAccount.balance); // Balance: $50
+  ronanAccount.credit(100);
+  print(ronanAccount.balance());
 
-  // try {
-  //   ronanAccount.withdraw(75); // This will throw an exception
-  // } catch (e) {
-  //   print(e); // Output: Insufficient balance for withdrawal!
-  // }
+  ronanAccount.withdraw(50);
+  print(ronanAccount.balance());
 
-  // try {
-  //   myBank.createAccount(100, 'Honlgy'); // This will throw an exception
-  // } catch (e) {
-  //   print(e); // Output: Account with ID 100 already exists!
-  // }
+  try {
+    ronanAccount.withdraw(75);
+  } catch (e) {
+    print(e);
+  }
+
+  try {
+    myBank.createAccount(100, 'Honlgy');
+  } catch (e) {
+    print(e);
+  }
 }
--- W2-S2-PRACTICE/EX-3.dart
+++ W2-S2-PRACTICE/EX-3.dart
@@ -1,27 +1,55 @@
-class MyDuration {
-  
+class CustomDuration {
+  final int _milliseconds;
 
-//   // Display the duration in a readable format
-//   @override
-//   String toString() {
-//     int seconds = (_milliseconds / 1000).round();
-//     int minutes = (seconds / 60).floor();
-//     seconds = seconds % 60;
-//     int hours = (minutes / 60).floor();
-//     minutes = minutes % 60;
-//     return '$hours hours, $minutes minutes, $seconds seconds';
-//   }
-// }
+  CustomDuration(this._milliseconds);
 
-// void main() {
-//   MyDuration duration1 = MyDuration.fromHours(3); // 3 hours
-//   MyDuration duration2 = MyDuration.fromMinutes(45); // 45 minutes
-//   print(duration1 + duration2); // 3 hours, 45 minutes, 0 seconds
-//   print(duration1>duration2); //true
+  CustomDuration.fromHours(int hours) : _milliseconds = hours * 3600000;
 
-//   try {
-//     print(duration2 - duration1); // This will throw an exception
-//   } catch (e) {
-//     print(e); 
-//   }
+  CustomDuration.fromMinutes(int minutes) : _milliseconds = minutes * 60000;
+
+  CustomDuration.fromSeconds(int seconds) : _milliseconds = seconds * 1000;
+
+  int get milliseconds {
+    return _milliseconds;
+  }
+
+  bool operator >(CustomDuration other) {
+    return this._milliseconds > other._milliseconds;
+  }
+
+  CustomDuration operator +(CustomDuration other) {
+    return CustomDuration(this._milliseconds + other._milliseconds);
+  }
+
+  CustomDuration operator -(CustomDuration other) {
+    int result = this._milliseconds - other._milliseconds;
+    if (result < 0) {
+      throw Exception('Duration cannot be negative');
+    }
+    return CustomDuration(result);
+  }
+
+  @override
+  String toString() {
+    int seconds = (_milliseconds / 1000).round();
+    int minutes = (seconds / 60).floor();
+    seconds = seconds % 60;
+    int hours = (minutes / 60).floor();
+    minutes = minutes % 60;
+    return '$hours hours, $minutes minutes, $seconds seconds';
+  }
+}
+
+void main() {
+  CustomDuration duration1 = CustomDuration.fromHours(3); // 3 hours
+  CustomDuration duration2 = CustomDuration.fromMinutes(45); // 45 minutes
+  CustomDuration duration3 = CustomDuration.fromSeconds(122); // 122 seconds
+  print(duration1 + duration2 + duration3); // 3 hours, 47 minutes, 2 seconds
+  print(duration1 > duration2); //true
+
+  try {
+    print(duration2 - duration1); // This will throw an exception
+  } catch (e) {
+    print(e);
+  }
 }

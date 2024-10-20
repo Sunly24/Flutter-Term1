e3855082932aeb0a82a56b2181110138b2cf2467
Sunly <129272504+Sunly24@users.noreply.github.com>
2024-10-20 12:43:25 +0700

    W2-S2-EX3

--- W2-S2-PRACTICE/EX-3.dart
+++ W2-S2-PRACTICE/EX-3.dart
@@ -1,32 +1,41 @@
-class CustomDuration {
+class MyDuration {
   final int _milliseconds;
 
-  CustomDuration(this._milliseconds);
+  MyDuration(this._milliseconds);
 
-  CustomDuration.fromHours(int hours) : _milliseconds = hours * 3600000;
+  MyDuration.fromHours(int hours) : _milliseconds = hours * 3600000;
 
-  CustomDuration.fromMinutes(int minutes) : _milliseconds = minutes * 60000;
+  MyDuration.fromMinutes(int minutes) : _milliseconds = minutes * 60000;
 
-  CustomDuration.fromSeconds(int seconds) : _milliseconds = seconds * 1000;
+  MyDuration.fromSeconds(int seconds) : _milliseconds = seconds * 1000;
 
   int get milliseconds {
     return _milliseconds;
   }
 
-  bool operator >(CustomDuration other) {
+  int cheackDuration() {
+    int milisec = _milliseconds;
+    if (milisec >= 0) {
+      return milisec;
+    } else {
+      throw Exception('Duration shall always be greater or equal to 0');
+    }
+  }
+
+  bool operator >(MyDuration other) {
     return this._milliseconds > other._milliseconds;
   }
 
-  CustomDuration operator +(CustomDuration other) {
-    return CustomDuration(this._milliseconds + other._milliseconds);
+  MyDuration operator +(MyDuration other) {
+    return MyDuration(this._milliseconds + other._milliseconds);
   }
 
-  CustomDuration operator -(CustomDuration other) {
+  MyDuration operator -(MyDuration other) {
     int result = this._milliseconds - other._milliseconds;
     if (result < 0) {
       throw Exception('Duration cannot be negative');
     }
-    return CustomDuration(result);
+    return MyDuration(result);
   }
 
   @override
@@ -41,9 +50,9 @@ class CustomDuration {
 }
 
 void main() {
-  CustomDuration duration1 = CustomDuration.fromHours(3); // 3 hours
-  CustomDuration duration2 = CustomDuration.fromMinutes(45); // 45 minutes
-  CustomDuration duration3 = CustomDuration.fromSeconds(122); // 122 seconds
+  MyDuration duration1 = MyDuration.fromHours(3); // 3 hours
+  MyDuration duration2 = MyDuration.fromMinutes(45); // 45 minutes
+  MyDuration duration3 = MyDuration.fromSeconds(122); // 122 seconds
   print(duration1 + duration2 + duration3); // 3 hours, 47 minutes, 2 seconds
   print(duration1 > duration2); //true
 

c18bbd0e40f4ae810c67b51101a366c57e1e8198
Sunly <129272504+Sunly24@users.noreply.github.com>
2024-11-16 21:03:54 +0700

    W6-S1

--- test_flutter/lib/W6-S1-PRACTICE/EXERCISE-2/main_start.dart
+++ test_flutter/lib/W6-S1-PRACTICE/EXERCISE-2/main_start.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: ProfileApp(
-      profileData: jinxProfile,
+      profileData: ronanProfile,
     ),
   ));
 }
